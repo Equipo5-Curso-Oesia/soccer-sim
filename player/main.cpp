21ca35bbbffd7cd7eb0e59348f7e51f86a35b317
MoTapiJok <gonta.m@hotmail.es>
2025-11-26 13:09:54 +0100

    main.cpp iniciado, jugador se conecta y se desconecta inmediatamente

--- player/main.cpp
+++ player/main.cpp
@@ -2,8 +2,44 @@
 
 using namespace std;
 
-int main()
+#include <MinimalSocket/udp/UdpSocket.h>
+
+// main with two args
+int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    // check if the number of arguments is correct
+    if (argc != 3)
+    {
+        cout << "Usage: " << argv[0] << " <team-name> <this-port>" << endl;
+        return 1;
+    }
+
+    // get the team name and the port
+    string team_name = argv[1];
+    MinimalSocket::Port this_socket_port = std::stoi(argv[2]);
+
+    cout << "Creating a UDP socket" << endl;
+
+    MinimalSocket::udp::Udp<true> udp_socket(this_socket_port, MinimalSocket::AddressFamily::IP_V6);
+
+    cout << "Socket created" << endl;
+
+    bool success = udp_socket.open();
+
+    if (!success)
+    {
+        cout << "Error opening socket" << endl;
+        return 1;
+    }
+
+    MinimalSocket::Address other_recipient_udp = MinimalSocket::Address{"127.0.0.1", 6000};
+    cout << "(init " + team_name + "(version 19))";
+
+    udp_socket.sendTo("(init " + team_name + "(version 19))", other_recipient_udp);
+    cout << "Init Message sent" << endl;
+
+    std::size_t message_max_size = 1000;
+    cout << "Waiting for a message" << endl;
+    auto received_message = udp_socket.receive(message_max_size);
+    std::string received_message_content = received_message->received_message;
 }
