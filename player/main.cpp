7cecab057e5080f3d3ee530251a23bc5d430ed0d
rodrimartruiz <rodrigo.martruiz@gmail.com>
2025-12-02 21:40:32 +0100

    git branch creation test

--- player/main.cpp
+++ player/main.cpp
@@ -13,10 +13,11 @@ Ejemplode como trabajar con git.
 1º Nunca se trabaja sobre la rama main (master).
 2º Se crea una rama nueva a partir de main (git branch <nombre de la rama nueva>).
 3º Se cambia a la nueva rama (git checkout <nombre de la rama nueva>).
-3º en ella se desarrolla la funcionalidad nueva, una unica.
+3º En ella se desarrolla la funcionalidad nueva, una unica.
 4º Cuando se termina, se hace un commit con un mensaje claro (git commit -a -m "mensaje claro").
-5º Se sube la rama al repositorio remoto (git push origin <nombre de la rama nueva>).
-6º Se abre un Pull Request en github para hacer el merge con la rama principal. Esto se hace con todo el equipo, nunca de forma individual.
+5º Se sube la rama al repositorio remoto (git push --set-upstream origin <nombre de la rama nueva>). Es recomendable que periodicamente se haga push mientras se trabaja para evitar perdida de trabajo y permitir que el resto pueda ver que haces.
+6º Si la rama ya está en el repositorio remoto, se hace (git push origin <nombre de la rama nueva>) o (git push).
+7º Se abre un Pull Request en github para hacer el merge con la rama principal. Esto se hace con todo el equipo, nunca de forma individual.
 
 /////////////////////////////////////////////////////////////////////////////////
 */
