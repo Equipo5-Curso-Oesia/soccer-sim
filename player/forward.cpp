74a8731d58a8034fb4dde275733b9258e3d3f9c1
JaimeVado <161128093+JaimeVado@users.noreply.github.com>
2025-12-16 19:52:55 +0100

    Update forward.cpp

--- player/forward.cpp
+++ player/forward.cpp
@@ -4,51 +4,75 @@
 #include <iostream>
 #include <functional>
 #include <queue>
+#include <list>
 
 using Task = std::function<void()>;
 
 void Forward::play(){
 
-    cout << "Forward playing" << endl;
+    cout << endl << endl;
 
-/*     Server& s = Server::getInstance();
+    Server& s = Server::getInstance();
     Field& f = Field::getInstance();
 
     static int i = 0;
+    static Server::GameState last_game_state = Server::GameState::unknown;
+    if (last_game_state != s.getState())
+        i = 0;
+    last_game_state = s.getState();
     if (s.getState() == Server::GameState::before_kick_off){ // Antes del saque
-        cout << i << endl;
-        turn(20);
-        i++;
+
+        switch(i) {
+            case 0:
+                move(posForNumber(player_number).first, posForNumber(player_number).second); 
+                break;
+            default:
+                findBall(i, get<1>(f.getBall()));
+                break;                 
+        }
+
     } else if ((s.getState() == Server::GameState::kick_off_l && side == 'l')
                     ||
                 s.getState() == Server::GameState::kick_off_r && side == 'r') { // Saco mi equipo
-
+        findBall(i, get<1>(f.getBall()));
     } else if ((s.getState() == Server::GameState::kick_off_l && side == 'r')
                     ||
                 s.getState() == Server::GameState::kick_off_r && side == 'l') { // Saca contrario
-
-
+        
+        findBall(i, get<1>(f.getBall()));
     } else if (s.getState() == Server::GameState::drop_ball) { // EL balon está en juego si en 100 ciclos el equipo que le toca sacar no la ha tocado o cualquiera la ha tocado posteriormente
 
     } else if (s.getState() == Server::GameState::play_on) { // El partido está en marcha
 
-        // Si ves la pelota a menos de x distancia
-            // Si la pelota está muy cerca
-                // Patear --> kick(50, dirPorteriaRival)
-            // Si la pelota no está muy cerca
-                // Mirar hacia ella --> turn(dirBall)
-                // Correr hacia ella --> dash(50)
-        // Si no ves la pelota
-            // Si no estás en la posición inicial
-                // Recuperar posición
-                    // turn(dirPosInicial)
-                    // dash(50)
-            // Una vez recuperada la posición
-                // Girar sobre si mismo --> turn(20)
+        PosData ball = f.getBall();
 
+        if (!get<1>(ball).has_value())
+            findBall(i, get<1>(f.getBall()));
+
+        else if(get<0>(ball).has_value()){
+            if(get<0>(ball).value() < 0.7) {
+
+                if (abs(get<1>(ball).value()) > 10.0) {
+                    turn(get<1>(ball).value());
+                    return;
+                }
+                kick(80, get<1>(ball).value()* -1);
+                return;
+            }
+            if (get<0>(ball).value() < 20.0) {
+                dash(70, get<1>(ball).value());
+                return;
+            }
+            // Track ball by turning toward it
+            turn(get<1>(ball).value());
+            return;
+        }
     }
 
-    static std::queue<Task> task_queue;
+
+    i++;
+
+/*  static std::queue<Task> task_queue;
 
     if (task_queue.empty()) {
         task_queue.push(std::bind(&Player::turnNeck, this, 10, false));
@@ -63,6 +87,21 @@ void Forward::play(){
 
     Task next_task = task_queue.front();
     next_task(); 
-    task_queue.pop(); */
+    task_queue.pop();
+*/
+}
 
+void Forward::findBall(int i, optional<double> ballDir)
+{
+    if (i % 3 == 0)
+    {
+        if (ballDir.has_value())
+        {
+            turn(ballDir.value());
+        }
+        else
+        {
+            turn(20);
+        }
+    }
 };
